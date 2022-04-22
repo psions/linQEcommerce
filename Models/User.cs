c3d484b8e70e329520fb5ae6923314dc79efbf30
psions <psionreality@gmail.com>
2022-04-22 14:45:33 -0500

    inital push

---
+++ Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DatabaseFirstLINQ.Models
+{
+    public partial class User
+    {
+        public User()
+        {
+            ShoppingCarts = new HashSet<ShoppingCart>();
+            Roles = new HashSet<Role>();
+        }
+
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime? RegistrationDate { get; set; }
+
+        public virtual ICollection<ShoppingCart> ShoppingCarts { get; set; }
+
+        public virtual ICollection<Role> Roles { get; set; }
+    }
+}
